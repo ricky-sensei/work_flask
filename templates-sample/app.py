08321410d9a8fa9aad65eee7915455dc9ea86a8a
ricky-sensei <axcelcamp.teacher@gmail.com>
2023-10-05 09:07:50 +0900

    条件分岐1終わり

--- templates-sample/app.py
+++ templates-sample/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, url_for
 
 
+class Hero():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+    def __str__(self):
+        return f"名前:{self.name} 年齢:{self.age}"
+
+
 # インスタンス作成
 app = Flask(__name__)
 
@@ -22,10 +32,40 @@ def item_detail(id):
 
 @app.route('/multiple')
 def show_jinja_multiple():
-    word1 = "テンプレートエンジン"
-    word2 = "神社"
-    return render_template('jinja/show1.html', temp=word1, jinja=word2)
+    # 辞書型で渡す
+    words={
+        "temp":"テンプレートえんじん",
+        "jinja":"じんじゃ"
+    }
+    return render_template('jinja/show2.html', key=words)
 
+@app.route('/list2')
+def show_jinja_list():
+    hero_list = ["桃太郎", "金太郎", "うらしまたろう"]
+    return render_template('jinja/show3.html', users=hero_list)
+
+
+@app.route('/class')
+def show_jinja_class():
+    hana = Hero("花咲かじいさん", 99)
+    return render_template('jinja/show4.html', user=hana)
+
+
+# 制御文
+# 商品クラス
+class Item(object):
+    """docstring for Item`."""
+    def __init__(self,id, name):
+        self.id = id
+        self.name = name
+    def __str__(self):
+        return f"商品ID:{self.id} 商品名:{self.name}"
+    
+
+@app.route('/for_list')
+def show_for_list():
+    item_list=[Item(1, "だんご"),Item(2, "にくまん"),Item(3, "どらやき"),]
+    return render_template("for_list.html", items = item_list)
 
 if __name__ == "__main__":
     app.run()
